18cc29005c6d039210c1872663817645a735c6d6
syy1997 <42967670+syy1997@users.noreply.github.com>
2018-10-14 13:46:57 -0400

    Update irtester.c

--- irtester.c
+++ irtester.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <wiringPi.h>
 
@@ -6,12 +7,27 @@ int main(int argc, char *argv[])
   int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+
   while(1) {
     printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
+    while(digitalRead(0) == 1){
+printf("Got a 1\n");
+digitalWrite(2,HIGH);
+digitalWrite(1,LOW);
+ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "syy1997", "EECS2031", "LAB4");
+}
     printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
-    printf("Alarm\n");
+    while(digitalRead(0) == 0){
+printf("Got a 0\n");
+digitalWrite(2,LOW);
+digitalWrite(1,HIGH);
+delay(300);
+digitalWrite(1,LOW);
+
+}
+  printf("Alarm\n");
   }
   /*NOTREACHED*/
   return 0 ;
